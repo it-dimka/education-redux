e35d93348e8bf09eeaa3145b9640606b0c7e8e75
Dmitriy <dimka_it@list.ru>
2022-06-23 23:08:28 +0300

    educationRedux

--- src/index.js
+++ src/index.js
@@ -1,17 +1,76 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import configureStore from "./store/store";
+import {
+  completeTask,
+  getTasks,
+  getTasksLoadingStatus,
+  loadTasks,
+  taskDeleted,
+  createTask,
+  titleChanged
+} from "./store/task";
+import {Provider, useDispatch, useSelector} from "react-redux";
+import {getErrors} from "./store/errors";
+
+const store = configureStore();
+
+const App = (params) => {
+  const state = [...useSelector(getTasks())];
+  const isLoading = useSelector(getTasksLoadingStatus());
+  const error = useSelector(getErrors());
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(loadTasks());
+  }, []);
+
+  const changeTitle = (taskId) => {
+    dispatch(titleChanged(taskId));
+  };
+
+  const removeTask = (taskId) => {
+    dispatch(taskDeleted(taskId));
+  };
+
+  if (isLoading) {
+    return <h1>Loading...</h1>;
+  }
+
+  if (error) {
+    return <p>{error}</p>;
+  }
+
+  const foo = {
+    title: new Date().getTime().toString(),
+    completed: false,
+  };
+
+  return (
+      <>
+        <h1>App</h1>
+        <button style={{backgroundColor: 'greenyellow'}} onClick={() => dispatch(createTask(foo))}>CreateTask</button>
+        <ul>
+          {state.reverse().map((el) => (
+              <li key={el.id}>
+                <p>{el.title}</p>
+                <p>{`Completed: ${el.completed}`}</p>
+                <button onClick={() => dispatch(completeTask(el.id))}>Complete</button>
+                <button onClick={() => changeTitle(el.id)}>ChangeTitle</button>
+                <button onClick={() => removeTask(el.id)}>Delete</button>
+                <hr/>
+              </li>
+          ))}
+        </ul>
+      </>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+      <Provider store={store}>
+        <App/>
+      </Provider>
+    </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
